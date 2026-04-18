fec06ee30ece9df37c8c34083e7281c4e14d5859
MiguellArcanjo <contato.miguelarcanjo2305@gmail.com>
2026-04-18 12:53:01 -0300

    Melhorando o client ssh

--- Clients/client_ssh.py
+++ Clients/client_ssh.py
@@ -1,11 +1,19 @@
 import paramiko
 
 host = "127.0.0.1"
-user = "..."
-passwd = "..."
+user = "kali"
+passwd = "kali"
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=user, password=passwd)
-stdin, stout, stderr = client.exec_command("ls")
-print(stout.readlines())
+
+
+while True:
+    stdin, stout, stderr = client.exec_command(input("Comando: "))
+    for line in stout.readlines():
+        print(line.strip())
+    
+    erros = stderr.readlines()
+    if erros:
+        print(erros)
